403bf0629535e6280cb04fd3b1e6d0c4b2a99caf
Teo Winkler <teo.winkler@gmail.com>
2020-03-05 15:28:30 +0100

    create basic  mobile layout

--- js/main.js
+++ js/main.js
@@ -1,74 +1,56 @@
-document.addEventListener('DOMContentLoaded', function () {
-	let textElement = document.getElementById("text").innerText;
+$(document).ready(function () { 
+    const $menuButton = $("#menu-button");
+    const $closeButton = $("#close-button");
+    const smallScreen = window.matchMedia("(max-width: 700px)");
 
-	// Convert inner text to an array of letters
-	let letterArray = textElement.split("");
+    // refresh on top
+    $(this).scrollTop(0);
 
-	// Inner text that will be modified later
-	let modifedText = textElement
+    //media queries
+    if (matchMedia){
+        smallScreen.addListener(shiftHeadline);
+        shiftHeadline(smallScreen);
+    };
 
-	// Text to insert at the end
-	let insert1 = "You can refresh this page";
-	let insert2 = "not our planet";
+    // manage headlines
+    function shiftHeadline(smallScreen) {
+        const $smallHeadline = $(".small-headline");
+        const $bigHeadline = $(".big-headline");
+        if (smallScreen.matches) {
+            $smallHeadline.addClass("active");
+            $bigHeadline.removeClass("active");
+        }
+        else {
+            $smallHeadline.removeClass("active");
+            $bigHeadline.addClass("active");
+    }
+    };
 
-	// Timer
-	let timer = (executable, time, original, replacement) => setTimeout(executable, time, original, replacement)
+    // button listeners
+    $menuButton.click(openMenu);
+    $closeButton.click(closeMenu);
 
-	// Filter and delete letters
-	function changeLetter(array, original, replace) {
-		return array.map(item => item.replace(original, replace))
-	}
+    // open & close menu
+    function openMenu() {
+        $("#menu").width("100%");
+    };
+    function closeMenu() {
+        $("#menu").width("0");
+    };
 
-	function eraseLetters(original, replacement) {
-		let newArray = changeLetter(letterArray, original, replacement);
-		let newText = newArray.join("");
-		
-		// modify both the array of letters and inner text
-		letterArray = newArray;
-		modifedText = newText;
+    // expand items
+    $(".toggle").click(function(e) {
+        e.preventDefault();
 
-		document.getElementById("text").innerText = newText;
-	}
-	
-	// Filter and delete words shorter than the given number
-	function filterWords(array, number) {
-		return array.filter(word => word.toString().length > number)
-	}
+      if ($(this).next().hasClass("show")) {
+          $(this).next().removeClass("show");
+          $(this).next().slideUp(350);
+      } else {
+        $(this).parent().parent().find("li .inner").removeClass("show");
+        $(this).parent().parent().find("li .inner").slideUp(350);
+        $(this).next().toggleClass("show");
+        $(this).next().slideToggle(350);
+      }
+  });
 
-	function eraseWords(original, replacement) {
-		let newArray = filterWords(modifedText.split(" "), original);
-		let newText = newArray.join(" ");
-
-		document.getElementById("text").innerText = newText;
-	}
-
-	// Show final text
-	function displayText(node, input, timer) {
-		setTimeout(() => {
-			document.getElementById(node).innerText = input
-		}, timer)
-	}
-
-	// Execute code
-	timer(eraseLetters, 7000, "m", "n");
-	timer(eraseLetters, 8000, "o", "s");
-	timer(eraseLetters, 9000, "b", "p");
-	timer(eraseLetters, 10000, "t", "l");
-	timer(eraseLetters, 10500, "e", "i");
-	timer(eraseLetters, 11000, "f", "c");
-	timer(eraseLetters, 11500, "a", "o");
-	timer(eraseLetters, 12000, "c", "k");
-
-	timer(eraseWords, 13000, 1, 0);
-	timer(eraseWords, 13500, 2, 0);
-	timer(eraseWords, 14000, 4, 0);
-	timer(eraseWords, 14500, 5, 0);
-	timer(eraseWords, 15000, 7, 0);
-	timer(eraseWords, 15500, 9, 0);
-	timer(eraseWords, 16000, 12, 0);
-	timer(eraseWords, 16500, 15, 0);
-
-	displayText("text", insert1, 18000);
-	displayText("text2", insert2, 20000);
-
-}, false);
+});
