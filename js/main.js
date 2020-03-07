73113de8025c67a5a5472a920e58c63798f44fb6
Teo Winkler <teo.winkler@gmail.com>
2020-03-07 18:51:32 +0100

    show menu level indicator with ::before

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
     // open & close menu
     function openMenu() {
-		$("#menu").slideDown(400, function () {
+		$("#menu").slideDown(350, function () {
 			$(this).css("display", "flex")
 		  });
 		$(".menu-content").animate({opacity:1}, 300);
@@ -12,24 +12,26 @@ $(document).ready(function () {
     function closeMenu() {
 		$(".menu-content").animate({opacity: 0}, 300);
 		$(".info-icons").animate({opacity: 0}, 300);
-		$("#menu").slideUp(400)
+		$("#menu").slideUp(300)
 	};
 	
 	// expand items
 	$(".toggle").click(function(e) {
 		e.preventDefault();
 
-
-      if ($(this).next().hasClass("show")) {
-          $(this).next().removeClass("show");
-          $(this).next().slideUp(350);
-	  } 
-	  else {
-        $(this).parent().parent().find("li .inner").removeClass("show");
-        $(this).parent().parent().find("li .inner").slideUp(350);
-        $(this).next().toggleClass("show");
-        $(this).next().slideToggle(350);
-	  }
+		if ($(this).next().hasClass("show")) {
+			$(this).next().removeClass("show");
+			$(this).next().slideUp(350);
+			$(this).removeClass("active-menu");
+		} 
+		else {
+			$(this).parent().parent().find("li .inner").removeClass("show");
+			$(this).parent().parent().find("li .inner").slideUp(350);
+			$(this).next().toggleClass("show");
+			$(this).next().slideToggle(350);
+			$(this).parents().find("li .toggle").removeClass("active-menu");
+			$(this).addClass("active-menu");	
+		}
   });
   
 	// Event listeners
